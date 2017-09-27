4978049e27321a2d9ed8739e6740f89304f67bf2
bquoctruong <brianquochungtruong@gmail.com>
2017-09-27 15:27:34 -0700

    Update CPSC386Project2Yahtze.py
    Running version of the game; implemented Three of a Kind
    What was done as of 9.27.2017 3:27

--- CPSC386Project2Yahtze.py
+++ CPSC386Project2Yahtze.py
@@ -5,7 +5,13 @@
 from pygame import *
 from random import *
 
-
+# class: player
+# Date of code (Last updated): 9/26
+# Programmer: Brian Truong
+# Description: class that tracks dices rolled, kept, and other such variables
+#               for the player when playing Yahtzee
+# Input: N/A
+# Output: N/A
 class player:
     #variables
     numberOfRolls = 0
@@ -13,6 +19,12 @@ class player:
     rollingDices = [1, 2, 3, 4, 5, 6]
     keptDice = [0, 0, 0, 0, 0, 0]
    
+    # Function: rollDices
+    # Date of code (Last updated): 9/25/2017
+    # Programmer: Brian Truong
+    # Description: uses randint to "roll" available dices
+    # Input: list(dices)
+    # Output: N/A
     def rollDices(dices):
         for x in range(len(dices)):
             if (dices[x] != 0):
@@ -20,12 +32,24 @@ class player:
         player.numberOfRolls += 1
         print("Roll number:" + str(player.numberOfRolls))
 
+    # Function:printDices
+    # Date of code (Last updated): 9/25
+    # Programmer: Brian Truong
+    # Description: prints chosen dices' values
+    # Input: list(dice), String
+    # Output: N/A
     def printDices(dice, string):
         print(string)
         for x in range(len(dice)):
             if (dice[x] != 0):
                 print("Dice", x+1, ":",dice[x])
 
+    # Function: keepDices
+    # Date of code (Last updated): 9/26/2017
+    # Programmer: Brian Truong
+    # Description: Allows player to keep dices for the purpose of Yahtzee
+    # Input: rolledDices, keptDice
+    # Output: N/A
     def keepDices(rolledDices, keptDice):
         decision = str
         player.printDices(rolledDices, "Rolled dices")
@@ -36,12 +60,12 @@ class player:
                     keptDice[x] = rolledDices[x]
                     rolledDices[x] = 0
     
-    
-
-
-
-
-
+# Class: Rules
+# Date of code (Last updated): 9/27/2017
+# Programmer: Brian Truong
+# Description: Given player dices, determines what points may be given to the player
+# Input: 
+# Output: 
 class Rules:
     aces = False
     twos = False
@@ -57,6 +81,12 @@ class Rules:
     yahtzee = False
     chance = False
 
+    # Function: runUpperSection
+    # Date of code (Last updated): 9/25/2017
+    # Programmer: Brian Truong
+    # Description: runs the set of rules (as functions) belonging to the upper section of the game Yahtzee
+    # Input: list(dice)
+    # Output: 
     def runUpperSection(dice):
         player.printDices(dice, "Kept dice")
         Rules.isAces(dice)
@@ -66,6 +96,12 @@ class Rules:
         Rules.isFives(dice)
         Rules.isSixes(dice)
 
+    # Function: isAces
+    # Date of code (Last updated): 9/25/2017
+    # Programmer: Brian Truong
+    # Description: Given a set of dice, determines if the dices have aces and sums up all "aces"
+    # Input: dice
+    # Output: 
     def isAces(dice):
         sum = 0
         for x in range(len(dice)):
@@ -74,6 +110,12 @@ class Rules:
         if (sum > 0):
                 print("You have Aces! Sum: ", sum)
 
+    # Function: isTwos
+    # Date of code (Last updated): 9/25/2017
+    # Programmer: Brian Truong
+    # Description: Given a set of dice, determines if the dices have twos and sums up all "twos"
+    # Input: dice
+    # Output: 
     def isTwos(dice):
         sum = 0
         for x in range(len(dice)):
@@ -82,6 +124,12 @@ class Rules:
         if (sum > 0):
                 print("You have Twos! Sum: ", sum)
 
+    # Function: isThrees
+    # Date of code (Last updated): 9/25/2017
+    # Programmer: Brian Truong
+    # Description: Given a set of dice, determines if the dices have threes and sums up all "threes"
+    # Input: dice
+    # Output: 
     def isThrees(dice):
         sum = 0
         for x in range(len(dice)):
@@ -90,6 +138,12 @@ class Rules:
         if (sum > 0):
                 print("You have Threes! Sum: ", sum)
 
+    # Function: isAces
+    # Date of code (Last updated): 9/25/2017
+    # Programmer: Brian Truong
+    # Description: Given a set of dice, determines if the dices have fours and sums up all "fours"
+    # Input: dice
+    # Output: 
     def isFours(dice):
         sum = 0
         for x in range(len(dice)):
@@ -98,6 +152,12 @@ class Rules:
         if (sum > 0):
                 print("You have Fours! Sum: ", sum)
 
+    # Function: isAces
+    # Date of code (Last updated): 9/25/2017
+    # Programmer: Brian Truong
+    # Description: Given a set of dice, determines if the dices have fives and sums up all "fives"
+    # Input: dice
+    # Output: 
     def isFives(dice):
         sum = 0
         for x in range(len(dice)):
@@ -106,6 +166,12 @@ class Rules:
         if (sum > 0):
                 print("You have Fives! Sum: ", sum)
 
+    # Function: isAces
+    # Date of code (Last updated): 9/25/2017
+    # Programmer: Brian Truong
+    # Description: Given a set of dice, determines if the dices have sixes and sums up all "sixes"
+    # Input: dice
+    # Output: 
     def isSixes(dice):
         sum = 0
         for x in range(len(dice)):
@@ -114,21 +180,60 @@ class Rules:
         if (sum > 0):
                 print("You have Sixes! Sum: ", sum)
     
-    #Fix, not sorting
+    # Function: isThreeOfAKind
+    # Date of code (Last updated): 9/27/2017
+    # Programmer: Brian Truong
+    # Description: To determine if the player rolled a three-of-a-kind
+    # Input: list(dice)
+    # Output: Boolean threeOfAKind
+    #9/27 NOTE: Functions okay; not consistent
     def isThreeOfAKind(dice):
         sum = 0
+        print("Begin sorting")
         dice.sort()
-        list = [0, 0, 0]
+        list = [0, 0, 0, 0, 0, 0]
+        print("Finish sorting")
+        player.printDices(dice, "Sorted dice")
+
+        #detects dice number and assigns them a slot in list
+        #does not store duplicates
+        print("Checking dices")
         for x in range(len(dice)):
-            while(dice[x+1] != None or dice[x+2] != None):
-                if (dice[x+1] == dice[x] + 1 and dice[x+2] == dice[x] +2):
-                    dice[x] = list[x]
-                    dice[x+1] = list[x+1]
-                    dice[x+2] = list[x+2]
-                    sum = dice[x] + dice[x+1] + dice[x+2]
-                    Rules.threeOfAKind = True
-        if (sum > 0):
-                print("You have Three of a Kind! Sum: ", sum)
+            if (dice[x] == 1):
+                list[0] = 1;
+            if (dice[x] == 2):
+                list[1] = 2;
+            if (dice[x] == 3):
+                list[2] = 3;
+            if (dice[x] == 4):
+                list[3] = 4;
+            if (dice[x] == 5):
+                list[4] = 5;
+            if (dice[x] == 6):
+                list[5] = 6;
+        
+        print("detecting three of a kind")
+        #detects three of a kind, going by possible triplet
+        if (list[3] == 4 and list[4] == 5 and list[5] == 6):
+            sum = list[3] + list[4] + list[5]
+            print("You have Three of a Kind! Sum: ", sum)
+            threeOfAKind = True
+            return threeOfAKind
+        elif(list[2] == 3 and list[3] == 4 and list[4] == 5):
+            sum = list[2] + list[3] + list[4]
+            print("You have Three of a Kind! Sum: ", sum)
+            threeOfAKind = True
+            return threeOfAKind
+        elif(list[1] == 2 and list[2] == 3 and list[3] == 4):
+            sum = list[1] + list[2] + list[3]
+            print("You have Three of a Kind! Sum: ", sum)
+            threeOfAKind = True
+            return threeOfAKind
+        elif(list[0] == 1 and list[1] == 2 and list[2] == 2):
+            sum = list[0] + list[1] + list[2]
+            print("You have Three of a Kind! Sum: ", sum)
+            threeOfAKind = True
+            return threeOfAKind
 
 
 
@@ -139,7 +244,9 @@ while (p1.numberOfRolls < 3 and p1.rollingDices != [0, 0, 0, 0, 0, 0]):
     p1.keepDices(p1.rollingDices, p1.keptDice)
 
 gameRules.runUpperSection(p1.keptDice)
+
 gameRules.isThreeOfAKind(p1.keptDice)
+
 
 
 
